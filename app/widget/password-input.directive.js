deba6606b425efcb7cdd7323f646626fd4626f58
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-10-08 17:03:18 +0200

    ok

---
+++ app/widget/password-input.directive.js
@@ -0,0 +1,34 @@
+(function () {
+    'use strict';
+
+    angular.module('widget')
+        .directive(`orsPasswordInput`, function () {
+            return {
+                templateUrl: `./widget/tmpl/ors-password-input.html`,
+                controller: ['$element', function OrsPasswordInputCtrl($element) {
+                    console.log('OrsPasswordInputCtrl start', $element);
+                    this.state = 'hidden';
+                    this.password = '';
+
+                    this.toggle = function () {
+                        if (this.state === 'hidden') {
+                            this.state = 'clear';
+                            setTimeout(() => {
+                                $element[0].querySelector('input[type=text]')
+                                    .focus();
+                            }, 0)
+                        } else {
+                            this.state = 'hidden';
+                            setTimeout(() => {
+                                $element[0].querySelector('input[type=password]')
+                                    .focus();
+                            }, 0)
+                        }
+
+                    }
+                }],
+                controllerAs: '$ctrl',
+            };
+        });
+
+})();
--- app/widget/widget.module.js
+++ app/widget/widget.module.js
@@ -1,36 +1,4 @@
 (function () {
     'use strict';
-
-    var app = angular.module('widget', []);
-
-
-    app.directive(`orsPasswordInput`, function () {
-        return {
-            templateUrl: `./widget/tmpl/ors-password-input.html`,
-            controller: ['$element', function OrsPasswordInputCtrl($element) {
-                console.log('OrsPasswordInputCtrl start', $element);
-                this.state = 'hidden';
-                this.password = '';
-
-                this.toggle = function () {
-                    if (this.state === 'hidden') {
-                        this.state = 'clear';
-                        setTimeout(() => {
-                            $element[0].querySelector('input[type=text]')
-                                .focus();
-                        }, 0)
-                    } else {
-                        this.state = 'hidden';
-                        setTimeout(() => {
-                            $element[0].querySelector('input[type=password]')
-                                .focus();
-                        }, 0)
-                    }
-
-                }
-            }],
-            controllerAs: '$ctrl',
-        };
-    });
-
+    angular.module('widget', []);
 })();
