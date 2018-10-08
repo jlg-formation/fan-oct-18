5d5ceb6a8f9a3acd408c802c10f6a05d26b1d661
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-10-08 15:21:13 +0200

    ok

--- app/widget/widget.module.js
+++ app/widget/widget.module.js
@@ -6,12 +6,21 @@
 
     app.directive(`orsPasswordInput`, function () {
         return {
-            templateUrl: `./widget/tmpl/ors-password-input.html`
+            templateUrl: `./widget/tmpl/ors-password-input.html`,
+            controller: function OrsPasswordInputCtrl() {
+                console.log('OrsPasswordInputCtrl start');
+                this.state = 'hidden';
+
+                this.toggle = function() {
+                    if (this.state === 'hidden') {
+                        this.state = 'clear';
+                    } else {
+                        this.state = 'hidden';
+                    }
+                }
+            },
+            controllerAs: '$ctrl',
         };
     });
-
-
-
-
 
 })();
