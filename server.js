a369a43bfd875d7f8f2e3c7b9ba7b71807cf8f6b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-10-10 11:16:50 +0200

    ok

--- server.js
+++ server.js
@@ -3,7 +3,27 @@
 const express = require('express'); // charge ExpressJS
 const serveIndex = require('serve-index');
 
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+
 const app = express();
+
+webpackConfig.output.path = '/';
+const compiler = webpack(webpackConfig);
+app.use('/app/wpk/', webpackDevMiddleware(compiler, {
+	stats: {
+        warningsFilter: function (str) {
+            if (str.match(/asset size limit/)) {
+                return true;
+            }
+            if (str.match(/webpack performance recommendations/)) {
+                return true;
+            }
+            return false;
+        }
+    }
+}));
 
 app.use(express.static('.'));
 app.use(serveIndex('.', { icons: true }));
