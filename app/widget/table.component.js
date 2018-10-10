f5dccffc832f1a7e70579f5f7fda4e3e4b629550
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-10-10 16:45:36 +0200

    ok

--- app/widget/table.component.js
+++ app/widget/table.component.js
@@ -2,6 +2,12 @@ export const table = {
     templateUrl: `./widget/tmpl/ors-table.html`,
     controller: function OrsTableCtrl() {
         console.log('orsTable start');
+
+        this.data = [
+            { id: 1, name: 'GUENEGO', description: 'hello', note: 12},
+            { id: 2, name: 'DURAND', description: 'coucou', note: 15},
+        ];
+        this.names = ['id', 'name', 'description', 'note'];
     }
 };
 
